02043c5e81f151b52b36d950cdd38dd3d62797ab
Crew Spence <csspence123@gmail.com>
2020-08-19 06:01:57 -0500

    Wrote working solution.

--- sum_of_lowest_positive_integers_codewars.js
+++ sum_of_lowest_positive_integers_codewars.js
@@ -6,6 +6,9 @@ For example, when an array is passed like [19, 5, 42, 2, 77], the output should 
 [10, 343445353, 3453445, 3453545353453] should return 3453455.
 */
 
-function sumTwoSmallestNumbers(numbers) {  
-  //Code here
+const sumTwoSmallestNumbers = (numbers) => {  
+  numbers.sort(function(a, b) {
+    return a - b;
+  });
+  return numbers[0] + numbers[1];
 }
